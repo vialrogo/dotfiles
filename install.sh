eecdd5ce5e2f487f7dc6ea8c54c2f1d45e1963cd
Victor Alberto Romero <vialrogo@gmail.com>
2021-08-27 17:48:57 -0300

    pip dependences addes to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Install extra nvim dependences
+pip install msgpack-python pynvim
+
 # Symbolic links
 ln -sf ${PWD}/bashrc        ~/.bashrc
 ln -sf ${PWD}/config/nvim   ~/.config/nvim
