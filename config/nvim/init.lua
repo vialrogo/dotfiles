2b2f9c74beec6a840a220697b569a32933fd71df
Victor <vialrogo@gmail.com>
2023-05-14 18:37:49 -0300

    Wraps for wiki files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -106,6 +106,7 @@ vim.g.neovide_hide_mouse_when_typing = false
 -- ------------------------------ Others ---------------------------------------
 vim.api.nvim_create_autocmd('Filetype', { pattern = 'wiki', command = [[WikiEnable]] })
 vim.api.nvim_create_autocmd('Filetype', { pattern = 'wiki', command = [[ListsEnable]] })
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'wiki', command = [[setlocal wrap]] })
 
 vim.api.nvim_create_autocmd("BufRead", { pattern = "*", command = [[normal zR]] })                  -- Set de fold to all open.
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = [[silent! lcd %:p:h]] })         -- Set the pwd to the open buffer.
