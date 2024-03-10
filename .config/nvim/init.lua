85765774e2ed7e0591b6bca0899f3e27764e5047
Victor Alberto Romero <vialrogo@gmail.com>
2024-03-10 19:35:35 -0300

    Complete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,4 @@
-vim.g.mapleader = " "
-
-require('lazy_conf')
-require('sets')
 require('keymaps')
+require('lazy_conf')
+require('options')
 require('neovide')
-
--- ----------------------- Look and feel options -----------------------------
-vim.cmd.colorscheme('ayu')
-
--- ------------------------------ Others ---------------------------------------
-vim.api.nvim_create_autocmd("BufRead", { pattern = "*", command = [[normal zR]] })                  -- Set de fold to all open.
-
-
-vim.api.nvim_set_hl(0, 'Conceal', { link = 'Normal' })                                              -- Link the Conceal highlight configuration to the Normal configuration.
