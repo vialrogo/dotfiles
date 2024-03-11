c6ab3a199d64170c54db7468f8255e99c4b27249
Victor Romero <vialrogo@gmail.com>
2024-03-11 09:49:31 -0300

    neovide font

--- .config/nvim/lua/neovide.lua
+++ .config/nvim/lua/neovide.lua
@@ -1,4 +1,4 @@
 if vim.g.neovide then
-    vim.opt.guifont = 'CaskaydiaCove Nerd Font Mono:h10'
+    vim.opt.guifont = 'CaskaydiaCove Nerd Font Mono:h14'
     vim.g.neovide_hide_mouse_when_typing = false
 end
