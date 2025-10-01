8b11e06be9bb927e82b5a556d7d8d1248adbcf10
Victor Alberto Romero <vialrogo@gmail.com>
2025-10-01 17:18:25 -0300

    Treesitter working!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,9 @@ require("config.lazy")
 require("mappings")
 require("autocmds")
 require("options")
+
+-- Load treesitter on file open
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+	branch = 'main',
+        build = ":TSUpdate",
+        
+	config = function()
+	    require("nvim-treesitter").setup ({
+
+	               -- enable auto install parsers
+	               auto_install = true,
+
+	               -- enable syntax highlighting
+	               highlight = { enable = true },
+
+	               -- enable indentation
+	               indent = { enable = true },
+
+	               -- enable autotagging (w/ nvim-ts-autotag plugin)
+	               autotag = { enable = true },
+
+	               incremental_selection = {
+	                   enable = true,
+	                   keymaps = {
+	                       init_selection = "<C-space>",
+	                       node_incremental = "<C-space>",
+	                       scope_incremental = false,
+	                       node_decremental = "<bs>",
+	                   },
+	               },
+
+	               -- Force use org rules for wiki files for no error message
+	               vim.treesitter.language.register("markdown", "wiki"),
+	           })
+	       end,
+    },
+}
