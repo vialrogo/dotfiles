7b1071b9a31a5addf8255532c6c078fa9fb94936
Victor Alberto Romero <vialrogo@gmail.com>
2025-10-01 15:44:46 -0300

    First 2 plugins working

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {'nvim-lua/plenary.nvim',
+                    'BurntSushi/ripgrep',
+                    'sharkdp/fd',
+		    'nvim-treesitter/nvim-treesitter',
+                    'nvim-tree/nvim-web-devicons'},
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+        { "<leader>fd", "<cmd>Telescope find_files cwd=/home/vialrogo/Wiki<cr>", desc = "Find Files in Wiki" },
+    },
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                path_display = { "truncate " },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+                        ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+                    },
+                },
+            },
+        })
+    end,
+}
