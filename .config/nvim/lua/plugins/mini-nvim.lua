fb766e2d35b70fe18d999ad1d4a19ad04c47770f
Victor Alberto Romero <vialrogo@gmail.com>
2025-10-01 22:04:59 -0300

    cmp again, and some plugins from general to mini-nvim option

---
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -0,0 +1,25 @@
+return{
+  'nvim-mini/mini.nvim',
+  version = false,
+  config = function()
+
+    require('mini.surround').setup({})
+    require('mini.pairs').setup({})
+    require('mini.icons').setup({})
+    require('mini.animate').setup({})
+    require('mini.cursorword').setup({})
+
+    local gen_loader = require('mini.snippets').gen_loader
+    require('mini.snippets').setup({
+      snippets = {
+        -- Load custom file with global snippets first (adjust for Windows)
+        gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+        -- Load snippets based on current language by reading files from
+        -- "snippets/" subdirectories from 'runtimepath' directories.
+        gen_loader.from_lang(),
+      },
+    })
+
+  end,
+}
