920289407afaf34192739d6422f9791d7dbea3a0
Victor Romero <vialrogo@gmail.com>
2025-11-06 12:17:03 -0300

    Remove all fold bullshit

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,5 +1,4 @@
--- Set the fold to all open.
-vim.api.nvim_create_autocmd("BufRead", { pattern = "*", command = [[normal zR]] })
-
 -- Link the Conceal highlight configuration to the Normal configuration.
-vim.api.nvim_set_hl(0, 'Conceal', { link = 'Normal' })
+vim.api.nvim_set_hl(0, "Conceal", { 
+  link = "Normal" 
+})
