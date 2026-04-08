db17466307b2abea0cffbbb6aee76b5ef33b4ba1
Victor Romero <vialrogo@gmail.com>
2026-04-08 20:26:24 -0300

    Change the branch from treesitter

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -2,7 +2,7 @@
 vim.pack.add({
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/nvim-telescope/telescope.nvim',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
   'https://github.com/jonarrien/telescope-cmdline.nvim',
   'https://github.com/nvim-tree/nvim-web-devicons',
 })
