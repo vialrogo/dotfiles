950e46e2159eae34f062c832516971e17d145b57
Victor Romero <vialrogo@gmail.com>
2026-04-08 20:26:24 -0300

    Change the branch from treesitter

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -1,46 +1,47 @@
 -- Add plugins
 vim.pack.add({
-  'https://github.com/nvim-treesitter/nvim-treesitter',
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
 })
 
 -- Setup
-require("nvim-treesitter").setup ({
-    -- enable auto install parsers
-    auto_install = true,
-    -- enable syntax highlighting
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
+require("nvim-treesitter").setup({
+  -- enable auto install parsers
+  auto_install = true,
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
     },
-    -- enable indentation
-    indent = { enable = true },
-    -- enable autotagging (w/ nvim-ts-autotag plugin)
-    autotag = { enable = true },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-        },
-    },
-    -- Force use org rules for wiki files for no error message
-    vim.treesitter.language.register("markdown", "wiki"),
+  },
+  -- Force use org rules for wiki files for no error message
+  vim.treesitter.language.register("markdown", "wiki"),
 })
 
 -- Update the languages on plugin update
-vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
-  local name, kind = ev.data.spec.name, ev.data.kind
-  if name == 'nvim-treesitter' and kind == 'update' then
-    if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
-    vim.cmd('TSUpdate')
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'nvim-treesitter' and kind == 'update' then
+      if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+      vim.cmd('TSUpdate')
+    end
   end
-end })
+})
 
 -- Load treesitter on file open
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
-
