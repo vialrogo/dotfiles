7c45e94fbf6d4aacdc7c2bc5d6d925c79e6fd96f
Victor Romero <vialrogo@gmail.com>
2026-04-21 22:19:16 -0300

    Cleanning the wiki pluging configs

--- .config/nvim/plugin/wiki-vim.lua
+++ .config/nvim/plugin/wiki-vim.lua
@@ -5,12 +5,3 @@ vim.pack.add({
 
 -- Set the root path
 vim.g.wiki_root = "/home/vialrogo/Wiki"
-
--- Auto load wiki functions on the file type
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "md",
-  callback = function()
-    vim.cmd("WikiEnable")
-    vim.opt_local.wrap = true
-  end,
-})
