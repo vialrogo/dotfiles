cdba112eb71bc8ec9550c20275c1062f7e6b65ff
Victor Romero <vialrogo@gmail.com>
2026-04-21 22:37:05 -0300

    Treesitter url fixed. First markdown plugin work in progress

--- .config/nvim/plugin/render-markdown.lua
+++ .config/nvim/plugin/render-markdown.lua
@@ -1,7 +1,7 @@
 -- Add plugins
 vim.pack.add({
   'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
+  'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/nvim-tree/nvim-web-devicons',
 })
 
@@ -10,22 +10,4 @@ require("render-markdown").setup({
   file_types = { "markdown" },
   render_modes = true,
   completions = { lsp = { enabled = true } },
-  heading = {
-    backgrounds = {
-      "RenderMarkdownH1Bg",
-      "RenderMarkdownH2Bg",
-      "RenderMarkdownH3Bg",
-      "RenderMarkdownH4Bg",
-      "RenderMarkdownH5Bg",
-      "RenderMarkdownH6Bg",
-    },
-    foregrounds = {
-      "RenderMarkdownH1",
-      "RenderMarkdownH2",
-      "RenderMarkdownH3",
-      "RenderMarkdownH4",
-      "RenderMarkdownH5",
-      "RenderMarkdownH6",
-    },
-  },
 })
