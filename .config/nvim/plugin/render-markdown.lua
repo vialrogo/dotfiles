ff546ef5d764171844e8ab97b072ece09d404b03
Victor Romero <vialrogo@gmail.com>
2026-04-22 00:17:45 -0300

    Configure custom checkbox icons and highlights for render-markdown
    Adds custom icons and highlight groups for unchecked, checked, and specialized todo states (todo, important, waiting) to enhance the markdown task list experience.

--- .config/nvim/plugin/render-markdown.lua
+++ .config/nvim/plugin/render-markdown.lua
@@ -8,6 +8,35 @@ vim.pack.add({
 -- Setup plugin
 require("render-markdown").setup({
   file_types = { "markdown" },
+  sign = { enabled = false },
   render_modes = true,
   completions = { lsp = { enabled = true } },
+  quote = { repeat_linebreak = true },
+  checkbox = {
+    unchecked = {
+      icon = '□ ',
+      highlight = 'RenderMarkdownInfo'
+    }, -- [ ]
+    checked = {
+      icon = '✘ ',
+      highlight = 'RenderMarkdownError'
+    }, -- [x]
+    custom = {
+      todo = {
+        raw = '[-]',
+        rendered = '✓ ',
+        highlight = 'RenderMarkdownTodo'
+      }, -- [-]
+      important = {
+        raw = '[~]',
+        rendered = '󰓎 ',
+        highlight = 'DiagnosticWarn',
+      }, -- [~]
+      waiting = {
+        raw = '[=]',
+        rendered = '󰥔 ',
+        highlight = 'RenderMarkdownHtmlComment',
+      }, -- [=]
+    },
+  },
 })
