d4a9244605aba96f70597a92187d8003fa7fc520
Victor Romero <vialrogo@gmail.com>
2026-05-11 13:18:27 -0300

    treesitter changed

--- .config/nvim/plugin/render-markdown.lua
+++ .config/nvim/plugin/render-markdown.lua
@@ -1,7 +1,6 @@
 -- Add plugins
 vim.pack.add({
   'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/nvim-tree/nvim-web-devicons',
 })
 
