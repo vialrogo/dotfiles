b0520c9c332cc0bf1ef421a700040a7d3686b8a6
Victor Romero <vialrogo@gmail.com>
2026-06-15 00:04:29 -0300

    A small neovim fix in a autocmd command

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,16 +1,19 @@
 -- Run on buffer write
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(event)
+    local can_format = vim.iter(vim.lsp.get_clients({ bufnr = event.buf }))
+      :any(function(client)
+        return client:supports_method("textDocument/formatting")
+      end)
+
     -- Format with LSP. Format only if supported
-    local clients = vim.lsp.get_clients({ bufnr = event.buf })
-    if #clients > 0 then
+    if can_format then
       vim.lsp.buf.format({
-        async = false,
         bufnr = event.buf,
+        async = false,
       })
     end
 
-    -- Trim whitespace
     local view = vim.fn.winsaveview()
     vim.cmd([[%s/\s\+$//e]])
     vim.fn.winrestview(view)
