7a887c4b63d48b6c5ffdfb4f408e5b86db035057
Victor Romero <vialrogo@gmail.com>
2026-07-28 22:42:38 -0300

    snipets colection configurated

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -1,12 +1,27 @@
 vim.pack.add({
   'https://github.com/nvim-mini/mini.nvim',
+  'https://github.com/rafamadriz/friendly-snippets',
 })
 
 if not vim.g.vscode then
   require('mini.icons').setup()
   require('mini.completion').setup()
   require('mini.pairs').setup()
-  require('mini.snippets').setup()
+
+  local snippets = require('mini.snippets')
+  snippets.setup({
+    snippets = {
+      snippets.gen_loader.from_lang(),
+    },
+    mappings = {
+      expand = '<C-j>',
+      jump_next = '<Tab>',
+      jump_prev = '<S-Tab>',
+      stop = '<C-c>',
+    },
+  })
+  snippets.start_lsp_server()
+
   require('mini.cmdline').setup()
 end
 
